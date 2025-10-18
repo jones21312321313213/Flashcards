ef208debf7b79087539c2e0b35d90529fb952097
jones21312321313213 <skinnyjones212121@gmail.com>
2025-10-18 17:53:27 +0800

    added login.html and index.html in user app

---
+++ flashcards/user/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.LandingPageView.as_view(),name='index'),
+    path('login/', views.LoginView.as_view(), name='login'),
+]
--- flashcards/user/views.py
+++ flashcards/user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+class LandingPageView(View):
+    template_name = 'index.html'
+
+    def get(self,request):
+        return render(request, self.template_name)
+
+class LoginView(View):
+    template_name = 'login.html'
+
+    def get(self,request):
+        return render(request, self.template_name)
+
