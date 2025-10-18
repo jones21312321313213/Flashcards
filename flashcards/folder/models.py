23c2a3eaa7463d6ed1453475fcfd121bb380a879
jones21312321313213 <skinnyjones212121@gmail.com>
2025-10-18 15:12:19 +0800

    fixed things in folder app

--- flashcards/folder/models.py
+++ flashcards/folder/models.py
@@ -6,7 +6,7 @@ class Folder(models.Model):
     folderId = models.AutoField(primary_key=True)
     name = models.CharField(max_length=120)
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null = True, blank = True)
 
     def __str__(self):
         return f"{self.folderId} {self.name} {self.created_at} {self.user.username}"
