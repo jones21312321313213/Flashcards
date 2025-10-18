8e2b5e87d78bfbc2938b296f29f3ba2a780c4d44
jones21312321313213 <skinnyjones212121@gmail.com>
2025-10-18 13:48:42 +0800

    added user app

---
+++ flashcards/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    userId = models.AutoField(primary_key=True) # or userId = models.IntegerField(primary_key=True) choose which is correct
+    username = models.CharField(max_length=100,null = False)
+    password = models.CharField(max_length=50,null = False)
+    email = models.TextField(max_length=100,null = False)
+    created_at = models.DateTimeField()
