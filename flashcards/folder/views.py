16e51a02013cb08e4f8999d26a20d726aa3394dd
John Kevin Muyco <johnkevinmuyco@Johns-MacBook-Air.local>
2025-10-19 14:12:57 +0800

    Creation of Folder

--- flashcards/folder/views.py
+++ flashcards/folder/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Folder
+
+# Display all folders
+def index(request):
+    folders = Folder.objects.all()
+    return render(request, 'folder/index.html', {'folders': folders})
+
+# Login page for folder app (if needed)
+def login_page(request):
+    return render(request, 'folder/login.html')
