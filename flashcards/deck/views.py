2f8a0001945a7a2f0708128789a44a638f8f382e
HPCS <saysonmichael11@gmail.com>
2025-10-19 16:24:24 +0800

    Add views for deck creation, listing, and updating - sayson

--- flashcards/deck/views.py
+++ flashcards/deck/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+#Deck
+class DeckCreateView(View):
+    template_name = 'todo'
+
+    def get(self, request):
+        return HttpResponse('creating deck')
+
+class DeckListView(View):
+    template_name = 'todo'
+
+    def get(self, request):
+        return HttpResponse('list of deck')
+class DeckUpdateView(View):
+    template_name = 'todo'
+
+    def get(self, request, pk):
+        return HttpResponse(f'Viewing deck {pk}')
