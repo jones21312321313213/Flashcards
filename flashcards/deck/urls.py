bcef606f2900c8d7bf75fb4da502154cb9a8ae93
HPCS <saysonmichael11@gmail.com>
2025-10-19 16:24:57 +0800

    Add files via upload

---
+++ flashcards/deck/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Deck
+    path('list/add', views.DeckCreateView.as_view(), name='deck_add'),
+    path('list', views.DeckListView.as_view(), name='deck_list'),
+    path('list/deck<int:pk>', views.DeckUpdateView.as_view(), name='deck_view'),
+
+
+]
